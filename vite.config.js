afcf96920d3172d643cce9b0a92dd43ee30edf13
Syed Anas Bukhari <anas14529@gmail.com>
2024-03-01 04:15:32 +0500

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,11 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    // Explicitly specify esbuild options
+    esbuild: {
+      jsxInject: `import React from 'react'`
+    }
   }
 })
